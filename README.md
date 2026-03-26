a19690616cef40aed5e5e21c4a7d9a85e4fd2e89
UilKarter <tenon123@gmail.com>
2026-03-26 22:12:46 +0300

    Step 4. Made compare logic, updated readme (asciinema)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/UilKarter/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/UilKarter/frontend-project-46/actions)
+## Gendiff asciinema
+[![asciicast](https://asciinema.org/a/iwoP8xzV9AlycEy3.svg)](https://asciinema.org/a/iwoP8xzV9AlycEy3)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import fs from 'fs'
 import path from 'path'
+import _ from 'lodash'
 
 export function genDiff(filepath1, filepath2) {
-  const cont1 = JSON.parse(fs.readFileSync(path.resolve(filepath1), 'utf-8'))
-  const cont2 = JSON.parse(fs.readFileSync(path.resolve(filepath2), 'utf-8'))
-  return `file1: ${JSON.stringify(cont1)}\nfile2: ${JSON.stringify(cont2)})`
+  const data1 = JSON.parse(fs.readFileSync(path.resolve(filepath1), 'utf-8'))
+  const data2 = JSON.parse(fs.readFileSync(path.resolve(filepath2), 'utf-8'))
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort()
+  const lines = keys.map((key) => {
+    if (!Object.hasOwn(data2, key)) return `  - ${key}: ${data1[key]}`
+    if (!Object.hasOwn(data1, key)) return `  + ${key}: ${data2[key]}`
+    if (data1[key] === data2[key]) return `    ${key}: ${data1[key]}`
+    return [
+      `  - ${key}: ${data1[key]}`,
+      `  + ${key}: ${data2[key]}`,
+    ];
+  }).flat()
+
+  return `{\n${lines.join('\n')}\n}`
 }
